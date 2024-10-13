8a9dd42919ad717f2f87ec0f2c29d0a7d42ecb4c
soph9i <berruvsophia@gmail.com>
2024-10-12 20:31:41 -0500

    A

--- src/main/java/pe/edu/cibertec/patitas_frontend_b/config/RestTemplateConfig.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_b/config/RestTemplateConfig.java
@@ -5,12 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    public RestTemplate restTemplateAutenticacion(RestTemplateBuilder builder) {
+        return builder
+                .rootUri("http://localhost:8081/autenticacion")
+                .setConnectTimeout(Duration.ofSeconds(30))
+                .setReadTimeout(Duration.ofSeconds(30))
+                .build();
     }
 
 }
